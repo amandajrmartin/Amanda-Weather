eab94103310b5dabd3e7d1595a256fc70a5d6883
amandajrmartin <martinamandajr@gmail.com>
2024-04-25 15:21:35 -0600

    final push

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,10 +17,12 @@ searchForm.addEventListener('submit', function (event) {
 });
 
 function getCurrentWeather(city) {
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-    `)
+    // fetch(`https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=${apiKey}
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}
+        `)
         .then(response => response.json())
         .then(data => {
+            console.log("Returned Data: ", data);
             const cityName = data.name;
             const date = new Date(data.dt * 1000).toLocaleDateString();
             const icon = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
@@ -70,7 +72,9 @@ function addToSearchHistory(city) {
     const listItem = document.createElement('div');
     listItem.textContent = city;
     listItem.classList.add('search-item');
-    listItem.addEventListener('click', function () {
+    listItem.addEventListener('click', function (event) {
+        console.log("Click...");
+        console.log("City: ", city);
         getCurrentWeather(city);
         getForecast(city);
     });
